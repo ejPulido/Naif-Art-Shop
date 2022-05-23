af718c67e3fcc2fcb1d40190dd41283cb88aac5c
= <=>
2022-05-23 17:31:45 -0400

    V1.0

---
+++ database.php
@@ -0,0 +1,5 @@
+<?php
+
+#data de base de datos local "localhost", "root","", "php_login_database"
+$conex =mysqli_connect("sql305.epizy.com", "epiz_31782094","cueEIrEdAe3dEJD", "epiz_31782094_php_login_database");
+?>
